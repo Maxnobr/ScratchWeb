64018a885f59f7d31581021fa31a243b77a1fd65
Maxnobr <maxnobrsasha@gmail.com>
2016-05-16 19:08:53 -0600

    try php

---
+++ LogIn.php
@@ -0,0 +1,12 @@
+﻿<!doctype html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Php thing</title>
+    </head>
+    
+    <body>
+        Welcome username : <?php echo $_POST["userid"]; ?><br>
+        Your password is : <?php echo $_POST["password"]; ?>
+    </body>
+</html>
